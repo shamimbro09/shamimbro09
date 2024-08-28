2c136613495201664bf46be22d5dd076a6f3ae30
shamimbro09 <shamim710261@gmail.com>
2024-08-29 00:33:28 +0600

    Add files via upload

--- main.java
+++ main.java
@@ -1,20 +1,29 @@
 class Shamim{
     public static void main(String[] args) {
-        double root1;
-        double root2;
-        double a=10.5,b=5,c=20;
-        double d=b*b-4*a*c;
-        if(d==0){
-            root1=root2=-b/2*a;
-            System.out.println(root1+" "+root2);
-        } else if (d>0) {
-            root1=-b+Math.sqrt(d)/2*a;
-            root2=-b-Math.sqrt(d)/2*a;
-            System.out.println(root1+" "+root2);
+        int mark=90;
+        switch (mark/10){
+            case 9:
+                System.out.println("Golden A+");
+                break;
+            case 8:
+                System.out.println("A+");
+                break;
+            case 7:
+                System.out.println("A");
+                break;
+            case 6:
+                System.out.println("B");
+                break;
+            case 5:
+                System.out.println("C");
+                break;
+            case 4:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
 
-        }
-        else{
-            System.out.println(" Root are imaginary");
-        }
     }
 }
